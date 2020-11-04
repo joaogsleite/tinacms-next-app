a0d5f893cc3c6b16a220634ba841b2baf43f0a46
joaogsleite <joao.leite@isobar.com>
2020-11-04 10:42:34 +0000

    fix domain

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
     API_BASE_URL: process.env.API_BASE_URL,
+    FRONTEND_DOMAIN: process.env.FRONTEND_DOMAIN,
   },
   async headers() {
     return [
@@ -13,7 +14,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: 'true' },
-          { key: "Access-Control-Allow-Origin", value: process.env.API_BASE_URL.split('://')[1] },
+          { key: "Access-Control-Allow-Origin", value: process.env.FRONTEND_DOMAIN },
           { key: "Access-Control-Allow-Methods", value: '*' },
           { key: "Access-Control-Allow-Headers", value: '*' },
         ]
